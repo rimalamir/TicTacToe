a92d8374a6a0dab230dbde86ae0916d102c80ead
Amir Rimal <amir.rimal007@gmail.com>
2022-05-05 15:25:25 +0545

    Added comments

--- TicTacToe.rb
+++ TicTacToe.rb
@@ -16,7 +16,6 @@ class Game
 
   def update_game (inputX, inputY)
     @game_state[inputX][inputY] = @turn
-
     @total_moves += 1
     render_game
 
@@ -39,7 +38,6 @@ class Game
     end
   end
 
-
   def check_valid_move?(inputX, inputY)
     @game_state[inputX][inputY] == "-"
   end
@@ -49,29 +47,26 @@ class Game
   end
 
   def check_game_over?(lastInputX, lastInputY)
-    #  CHECK THAT ROW
+    #  CHECK THAT ROW, IF ALL ELEMENTS IN GIVEN ROW ARE SAME, GAME OVER
     return true if !@game_state[lastInputX].any? {|elem| elem != @turn}
 
-    #CHECK THAT COLUMN
+    #CHECK THAT COLUMN, IF ALL ELEMENTS IN GIVEN COLUMN ARE SAME, GAME OVER
     return true if !@game_state.transpose[lastInputY].any? {|elem| elem != @turn }
 
+    #CHECK THAT DIAGONAL, ONLY IF LAST ENTERED ELEMENT FALLS ON MAIN DIAGONAL
     if lastInputX == lastInputY
-      #CHECK THAT DIAGONAL
-      main_diagonal_elements = (0 ... @size).collect { |element| @game_state[element][element] }
+      main_diagonal_elements = (0 ... @size).map { |element| @game_state[element][element] }
       return true if !main_diagonal_elements.any? {|a| a != @turn }
     end
 
-    #@game_state.transpose.map(&:reverse)[element][element]
+    #CHECK OPPOSITE DIAGONAL, ONLY IF LAST ENTERED ELEMENT FALLS ON OPPOSITE DIAGONAL
     if lastInputX + lastInputY == @size - 1
-      #CHECK OPPOSITE DIAGONAL
-      opposite_diagonal_elements = (0 ... @size).collect { |element| @game_state.transpose.map(&:reverse)[element][element]}
+      opposite_diagonal_elements = (0 ... @size).map { |element| @game_state.transpose.map(&:reverse)[element][element]}
       return true  if !opposite_diagonal_elements.any? {|a| a != @turn}
     end
 
     return false
-
   end
-
 end
 
 #Sanitaize the input, remove non numeric characters, extra spaces and return first two numeric entries with 0 starting position
@@ -79,6 +74,7 @@ def format_input(move)
   move.split(/\D/).join(' ').gsub(/\s+/, " ").strip.split( " ").first(2).map {|num| num.to_i - 1}
 end
 
+#PROGRAM STARTS HERE
 print "ENTER SIZE FOR GAME: no. of rows:  "
 size = gets.to_i
 
@@ -96,8 +92,7 @@ while !new_game.game_over
       puts "INPUT = #{formatted_input.map { |num| num + 1 }}"
       new_game.update_game(formatted_input.first, formatted_input.last)
     else
-      puts "INVALID MOVE, CELL ALREADY TAKEN UP"
+      puts "INVALID MOVE, CELL ALREADY FILLED"
     end
     end
 end
-
