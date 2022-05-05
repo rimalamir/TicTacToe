81d8100dbaa9208300a4baa986ba41c47f02b9bb
Amir Rimal <amir.rimal007@gmail.com>
2022-05-05 17:04:55 +0545

    Added linter and fixed as per convention

--- TicTacToe.rb
+++ TicTacToe.rb
@@ -1,87 +1,123 @@
+# frozen_string_literal: true
 require 'io/console'
 
+### A Game class that assigns players, keeps track of game states, and validates user input
 class Game
   attr_accessor :game_over
-  def initialize (size)
+
+  def initialize(size)
     @size = size
-    @player_one = "X"
-    @player_two = "O"
-    @game_state =  Array.new(size){Array.new(size) { "-" }}
+    @player_one = 'X'
+    @player_two = 'O'
+    @game_state = Array.new(size) { Array.new(size) { '-' } }
     @turn = @player_one
     @game_over = false
     @total_moves = 0
     render_game
-    puts @turn == @player_one ?  "PLAYER ONE'S TURN" : "PLAYER TWO'S TURN" if !@game_over
+    puts @turn == @player_one ? 'PLAYER ONE\'S TURN' : 'PLAYER TWO\'S TURN' unless @game_over
   end
 
-  def update_game (inputX, inputY)
-    @game_state[inputX][inputY] = @turn
+  def update_game(input_x, input_y)
+    @game_state[input_x][input_y] = @turn
     @total_moves += 1
     render_game
-
-    @game_over = check_game_over?(inputX, inputY)
-    puts "GAME OVER" if @game_over
-    puts @turn == @player_one ?  "PLAYER ONE WINS" : "PLAYER TWO WINS" if @game_over
-    return  if @game_over #GAME OVER BY WIN OR LOSE
-    switch_turn
-    @game_over = @total_moves == (@size * @size) #GAME OVER BY TIE
-    puts "GAME TIED" if @game_over
-    puts @turn == @player_one ?  "PLAYER ONE'S TURN" : "PLAYER TWO'S TURN" if !@game_over
+    handle_game_over_case(input_x, input_y)
   end
+
+  def handle_game_over_case(input_x, input_y)
+    @game_over = check_game_over?(input_x, input_y)
+    puts 'GAME OVER' if @game_over
+    puts @turn == @player_one ? 'PLAYER ONE WINS' : 'PLAYER TWO WINS' if @game_over
+    return if @game_over # GAME OVER BY WIN OR LOSE
+
+    switch_turn
+    check_for_tie
+    puts @turn == @player_one ?  'PLAYER ONE\'S TURN' : 'PLAYER TWO\'S TURN' unless @game_over
+  end
+
+  private
+
+  def check_for_tie
+    @game_over = @total_moves == (@size * @size) # GAME OVER BY TIE
+    puts 'GAME TIED' if @game_over
+  end
+
+  private
 
   def render_game
     @size.times do |index|
       @size.times do |internal_index|
-        print @game_state[index][internal_index] + "  "
+        print "#{@game_state[index][internal_index]} "
       end
-      print"\n"
+      print "\n"
     end
   end
 
-  def check_valid_move?(inputX, inputY)
-    @game_state[inputX][inputY] == "-"
+  public
+
+  def check_valid_move?(input_x, input_y)
+    @game_state[input_x][input_y] == '-'
   end
 
   def switch_turn
     @turn = @turn == @player_one ? @player_two : @player_one
   end
 
-  def check_game_over?(lastInputX, lastInputY)
-    #  CHECK THAT ROW, IF ALL ELEMENTS IN GIVEN ROW ARE SAME, GAME OVER
-    return true if !@game_state[lastInputX].any? {|elem| elem != @turn}
+  def check_game_over?(last_input_x, last_input_y)
+    # CHECK THAT ROW, IF NONE OF THE ELEMENT IS DIFFERENT  GAME OVER
+    return true if game_over_in_row? last_input_x
 
-    #CHECK THAT COLUMN, IF ALL ELEMENTS IN GIVEN COLUMN ARE SAME, GAME OVER
-    return true if !@game_state.transpose[lastInputY].any? {|elem| elem != @turn }
+    # CHECK THAT COLUMN, IF NONE OF THE ELEMENT IS DIFFERENT GAME OVER
+    return true if game_over_in_column? last_input_y
 
-    #CHECK THAT DIAGONAL, ONLY IF LAST ENTERED ELEMENT FALLS ON MAIN DIAGONAL
-    if lastInputX == lastInputY
-      main_diagonal_elements = (0 ... @size).map { |element| @game_state[element][element] }
-      return true if !main_diagonal_elements.any? {|a| a != @turn }
-    end
+    # CHECK THAT DIAGONAL, ONLY IF LAST ENTERED ELEMENT FALLS ON MAIN DIAGONAL
+    return true if (last_input_x == last_input_y) && game_over_in_main_diagonal?
 
-    #CHECK OPPOSITE DIAGONAL, ONLY IF LAST ENTERED ELEMENT FALLS ON OPPOSITE DIAGONAL
-    if lastInputX + lastInputY == @size - 1
-      opposite_diagonal_elements = (0 ... @size).map { |element| @game_state.transpose.map(&:reverse)[element][element]}
-      return true  if !opposite_diagonal_elements.any? {|a| a != @turn}
-    end
+    # CHECK OPPOSITE DIAGONAL, ONLY IF LAST ENTERED ELEMENT FALLS ON OPPOSITE DIAGONAL
+    return true if (last_input_x + last_input_y == @size - 1) && game_over_in_opposite_diagonal?
 
-    return false
+    false
   end
+
+  private
+
+  def game_over_in_row?(input_x)
+    @game_state[input_x].none? { |elem| elem != @turn }
+  end
+
+  private
+
+  def game_over_in_column?(input_y)
+    @game_state.transpose[input_y].none? { |elem| elem != @turn }
+  end
+
+  private
+
+  def game_over_in_main_diagonal?
+    main_diagonal_elements = (0...@size).map { |element| @game_state[element][element] }
+    main_diagonal_elements.none? { |a| a != @turn }
+  end
+
+  def game_over_in_opposite_diagonal?
+    opposite_diagonal_elements = (0...@size).map { |element| @game_state.transpose.map(&:reverse)[element][element] }
+    opposite_diagonal_elements.none? { |a| a != @turn }
+  end
+
 end
 
-#Sanitaize the input, remove non numeric characters, extra spaces and return first two numeric entries with 0 starting position
+# Sanitize the input, remove non numeric characters, extra spaces and return first two numeric entries with 0 starting position
 def format_input(move)
-  move.split(/\D/).join(' ').gsub(/\s+/, " ").strip.split( " ").first(2).map {|num| num.to_i - 1}
+  move.split(/\D/).join(' ').gsub(/\s+/, ' ').strip.split(' ').first(2).map { |num| num.to_i - 1}
 end
 
-#PROGRAM STARTS HERE
+# PROGRAM STARTS HERE
 # print "ENTER SIZE FOR GAME: no. of rows:  "
-size = 3#gets.to_i
+size = 3 # gets.to_i
 
 new_game = Game.new size
 
-while !new_game.game_over
-  print "Enter position for new move: row, column:  "
+until new_game.game_over
+  print 'Enter position for new move: row, column:  '
   move = gets
   formatted_input = format_input(move)
   if !(formatted_input.all? { |num| num.between?(0, size - 1)})
@@ -92,7 +128,7 @@ while !new_game.game_over
       puts "INPUT = #{formatted_input.map { |num| num + 1 }}"
       new_game.update_game(formatted_input.first, formatted_input.last)
     else
-      puts "INVALID MOVE, CELL ALREADY FILLED"
+      puts 'INVALID MOVE, CELL ALREADY FILLED'
     end
     end
 end
